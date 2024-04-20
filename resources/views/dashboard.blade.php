ad160c860ce9ab1ac19c757038cdcf973e32f0c7
Cyrus998 <jonathanfranciscosopro@gmail.com>
2024-04-21 04:16:10 +0800

    It didn't have to be magic.

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('User Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="p-6 text-gray-900 dark:text-gray-100">
+            Content Starts Here!
+        </div>
+    </div>
+</x-app-layout>
